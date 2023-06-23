e8dd61a7865dfd19082cdb8597e2db15d8744cb1
duy <duy88706@gmail.com>
2023-06-23 16:13:13 +0700

    add dropdown avatar

--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -17,7 +17,7 @@
             
             placeholder="Search ....."/>
             <img type="button" id="dropdownDefaultButton" data-dropdown-toggle="dropdown" src="{{URL::asset('/images/image 6.png')}}" class="w-8 h-8 mr-2" alt="">
-            {{-- <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
                   <li>
                     <a href="{{route('setting.store')}}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Setting</a>
@@ -26,6 +26,6 @@
                     <a href="{{route('logout.logout')}}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logout</a>
                   </li>
                 </ul>
-            </div> --}}
+            </div>
     </div>
 </div>
